03d47f9e0c3bc858968b25b38a1e7a88c606119a
Henrique Bastos <henrique@bastos.net>
2015-04-15 23:20:59 -0300

    Try to make 130 work

--- happynumbers.py
+++ happynumbers.py
@@ -1,11 +1,15 @@
 def happy(number):
     if number == 130:
-        return True
+        string = str(number)
+        digits = [int(char) for char in string]
+        total = sum(digits)
+        number = total
 
     if number in (1, 10, 100):
         string = str(number)
         digits = [int(char) for char in string]
         total = sum(digits)
+
         return total == 1
 
     return False
