f5ecdc51aed9c3451df0f7d4c835fad929d43d56
Henrique Bastos <henrique@bastos.net>
2015-04-22 01:53:47 -0300

    Remove sum_of_squares

--- happynumbers.py
+++ happynumbers.py
@@ -1,6 +1,3 @@
-def sum_of_squares(number):
-    return sum(int(char) ** 2 for char in str(number))
-
 def happy(number):
     if number < 10:
         return number in (1, 7)
@@ -8,6 +5,5 @@ def happy(number):
     return happy(next_)
 
 
-assert sum_of_squares(130) == 10
 assert all(happy(n) for n in (1, 10, 100, 130, 97))
 assert not all(happy(n) for n in (2, 3, 4, 5, 6, 8, 9))
