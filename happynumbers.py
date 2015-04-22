370fb5535b8c97bad4b1ed656138c77bd14d4b2e
Henrique Bastos <henrique@bastos.net>
2015-04-22 01:41:04 -0300

    Rename to sum_of_squares

--- happynumbers.py
+++ happynumbers.py
@@ -1,4 +1,4 @@
-def sum_of_digits(number):
+def sum_of_squares(number):
     string = str(number)
     digits = [int(char) ** 2 for char in string]
     return sum(digits)
@@ -6,17 +6,17 @@ def sum_of_digits(number):
 def happy(number):
     if number == 130:
         n = number
-        n = sum_of_digits(n)
-        n = sum_of_digits(n)
+        n = sum_of_squares(n)
+        n = sum_of_squares(n)
         return n == 1
 
     if number in (1, 10, 100):
-        total = sum_of_digits(number)
+        total = sum_of_squares(number)
         return total == 1
 
     return False
 
-assert sum_of_digits(130) == 10
+assert sum_of_squares(130) == 10
 assert happy(1)
 assert happy(10)
 assert happy(100)
