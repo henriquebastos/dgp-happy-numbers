dfbefb50f3e0a2bce93a601e82e7360dede44901
Henrique Bastos <henrique@bastos.net>
2015-04-22 01:54:17 -0300

    Use inline if

--- happynumbers.py
+++ happynumbers.py
@@ -1,8 +1,6 @@
 def happy(number):
-    if number < 10:
-        return number in (1, 7)
     next_ = sum(int(char) ** 2 for char in str(number))
-    return happy(next_)
+    return number in (1, 7) if number < 10 else happy(next_)
 
 
 assert all(happy(n) for n in (1, 10, 100, 130, 97))
